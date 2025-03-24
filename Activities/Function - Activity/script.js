fed6f9fd25a392b45250e56888289b49f3fb6912
BalasabasVenz <vbalasabas@filamer.edu.ph>
2025-03-24 15:06:51 +0800

    Function Activity 1

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,13 @@
+function whatShallIWear(temp) {
+    if (temp < 60) {
+        console.log('Wear a Jacket');
+    }else if (temp < 70) {
+        console.log('Wear a Sweater');
+    }else {
+        console.log('Wear a t-shirt');
+    }
+}
+
+whatShallIWear(50)
+whatShallIWear(109)
+whatShallIWear(60)
